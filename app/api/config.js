6685062a9fec58a245dffce59bedde149607c456
AvArtAlek <159720522+AvArtAlek@users.noreply.github.com>
2024-05-31 12:03:37 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://saintedlittlebackend.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://backend-test-domen.nomorepartiesco.ru//api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
